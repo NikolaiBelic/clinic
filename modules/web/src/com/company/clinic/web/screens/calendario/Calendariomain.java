ce8caccc874f3ecc51ce72430e61d4be6ee02569
Álvaro Nicolás Climent <akralan1414@gmail.com>
2025-06-15 19:08:32 +0200

    ajuste hora calendario 15

--- modules/web/src/com/company/clinic/web/screens/calendario/Calendariomain.java
+++ modules/web/src/com/company/clinic/web/screens/calendario/Calendariomain.java
@@ -154,9 +154,9 @@ public class Calendariomain extends Screen {
         calendario.setTimeFormat(Calendar.TimeFormat.FORMAT_24H);
         calendario.setFirstVisibleHourOfDay(10);
         calendario.setLastVisibleHourOfDay(20);
-        calendario.setFirstDayOfWeek(java.util.Calendar.MONDAY);  // Usando java.util.Calendar (valor 2)
-        calendario.setFirstVisibleDayOfWeek(java.util.Calendar.MONDAY);
-        calendario.setLastVisibleDayOfWeek(java.util.Calendar.FRIDAY);
+//        calendario.setFirstDayOfWeek(2);  // Usando java.util.Calendar (valor 2)
+        calendario.setFirstVisibleDayOfWeek(3);
+        calendario.setLastVisibleDayOfWeek(7);
         /*calendario.setFirstVisibleDayOfWeek(2);
         calendario.setLastVisibleDayOfWeek(6);*/
         calendario.setWeeklyCaptionFormat("dd/MM/yyyy");
